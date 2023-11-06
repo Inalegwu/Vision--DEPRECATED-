a62b511b3a52555bafa6d60c990515836499b2d2
Inalegwu Ikwue <ikwueinalegwu@gmail.com>
2023-11-06 16:48:21 +0000

    remove previous rar and zip libraries

--- src/shared/routers/library.ts
+++ src/shared/routers/library.ts
@@ -1,7 +1,5 @@
 import { app, dialog } from "electron";
 import { publicProcedure, router } from "../../trpc";
-import * as fs from "fs/promises";
-import { ArchiveReader, libarchiveWasm } from "libarchive-wasm";
 
 export const libraryRouter = router({
   addToLibrary: publicProcedure.mutation(async () => {
@@ -20,14 +18,6 @@ export const libraryRouter = router({
     }
 
     console.log(result.filePaths);
-
-    const file = await fs.readFile(result.filePaths[0]);
-    const mod = await libarchiveWasm();
-    const reader = new ArchiveReader(mod, new Int8Array(file));
-
-    for (const result of reader.entries()) {
-      console.log(result);
-    }
   }),
 });
 
