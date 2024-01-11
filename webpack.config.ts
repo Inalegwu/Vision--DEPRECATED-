f852b80710e67e98ceae6a903b64d0dc146622a5
Inalegwu Ikwue <54356020+Inalegwu@users.noreply.github.com>
2024-01-11 02:46:48 +0100

    fuckkkkk

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,8 @@
 import CopyPlugin from "copy-webpack-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from "path";
 import { Configuration } from "webpack";
-lugin;
-from;
-("mini-css-extract-plugin");
 
 const isDev = process.env.NODE_ENV === "development";
 
@@ -54,6 +52,10 @@ const common: Configuration = {
           from: "node_modules/better-sqlite3/",
           to: "node_modules/better-sqlite3/",
         },
+        // {
+        //   from: "./.drizzle/",
+        //   to: "./.drizzle/",
+        // },
       ],
     }),
   ],
